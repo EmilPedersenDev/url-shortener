16bf71c3a89b8ddba4b58717b52a1fa7158cbe07
Emil <emil.pedersen@refined.com>
2025-05-26 21:48:36 +0200

    rr

--- src/server/common/config/database-client.ts
+++ src/server/common/config/database-client.ts
@@ -1,6 +1,6 @@
 import knex from 'knex';
 import knexConfiguration from '../../../../knexfile';
 
-console.log(knexConfiguration);
+console.log('sdfsdfs', knexConfiguration, process.env.DATABASE_PASSWORD);
 
 export const knexClient: knex.Knex<never, []> = knex(knexConfiguration[process.env.NODE_ENV ?? 'development']);
